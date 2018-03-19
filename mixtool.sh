a677c0838150ff08cacf062ee0bd7e409c2cabb1
Bachsau <dev@bachsau.com>
2018-03-19 21:11:52 +0100

    Add test launcher

---
+++ mixtool.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+trap false INT
+cd "$(dirname "$0")"
+export 'LC_ALL'='C'
+export 'G_SLICE'='always-malloc'
+do_quit=''
+while test -z "$do_quit"
+do
+	clear
+	printf 'Launching Mixtool...\n' >&2
+	python3 -BEs __main__.py "$@"
+	printf 'Mixtool has quit with \e[1mexit code %b\e[0m.\n' $? >&2
+	read -rn 1 -p '=> Press return to restart or any other key to exit.' do_quit
+done
