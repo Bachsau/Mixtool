0f8afb72d83835c41ed9ee6b7022c8fa5eb93517
Bachsau <dev@bachsau.com>
2018-04-15 11:11:06 +0200

    Remove Zenity from launcher

--- mixtool.sh
+++ mixtool.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 trap false INT
-export LC_ALL='en_US.UTF-8'
-export G_SLICE='debug-blocks'
 appdir="$(dirname "$(realpath "$0")")"
+export LC_ALL=en_US.UTF-8
+export G_SLICE=debug-blocks
 printf 'Launching Mixtool... %s\n' "$(date '+%F %T %Z')" >>"$appdir/output.log"
 python3 -BEsu "$appdir/src/__main__.py" "$@" >>"$appdir/output.log" 2>&1
 printf 'Mixtool has quit with EXIT CODE %u.\n\n' $? >>"$appdir/output.log"
-if [[ -n "$(command -v zenity)" && (-n "$(command -v xdg-open)" || -n "$(command -v open)") ]]
-then
-	zenity --question --no-wrap --no-markup --text 'Open "debug.log"?' --ok-label 'Yes' --cancel-label 'No' >'/dev/null' 2>&1
-	if [[ $? -eq 0 ]]
-	then
-		unset LC_ALL G_SLICE
-		if [[ -n "$(command -v xdg-open)" ]]
-		then
-			exec xdg-open "$appdir/output.log"
-		else
-			exec open "$appdir/output.log"
-		fi
-	fi
+unset LC_ALL G_SLICE
+if [ -n "$(command -v xdg-open)" ]; then
+	exec xdg-open "$appdir/output.log"
+elif [ -n "$(command -v open)" ]; then
+	exec open "$appdir/output.log"
 fi
