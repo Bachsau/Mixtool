ba96c1968baae52f929314815095eef6aad3d261
Bachsau <dev@bachsau.com>
2018-04-18 18:19:34 +0200

    Add process ID to launcher output to help with D-Bus activation

--- mixtool.sh
+++ mixtool.sh
@@ -3,9 +3,9 @@ trap false INT
 appdir="$(dirname "$(realpath "$0")")"
 export LC_ALL=en_US.UTF-8
 export G_SLICE=debug-blocks
-printf 'Launching Mixtool... %s\n' "$(date '+%F %T %Z')" >>"$appdir/output.log"
+printf '\n[%u] Launching Mixtool... %s\n' $$ "$(date '+%F %T %Z')" >>"$appdir/output.log"
 python3 -BEsu "$appdir/src/__main__.py" "$@" >>"$appdir/output.log" 2>&1
-printf 'Mixtool has quit with EXIT CODE %u.\n\n' $? >>"$appdir/output.log"
+printf '[%u] Mixtool has quit with EXIT CODE %u.\n\n' $$ $? >>"$appdir/output.log"
 unset LC_ALL G_SLICE
 if [ -n "$(command -v xdg-open)" ]; then
 	exec xdg-open "$appdir/output.log"
