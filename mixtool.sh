942e6248e6e273b0f4bd2e5b35cd1e04321469d0
Bachsau <dev@bachsau.com>
2018-04-24 05:11:23 +0200

    Stop launcher from opening "output.log"

--- mixtool.sh
+++ mixtool.sh
@@ -6,9 +6,3 @@ export G_SLICE=debug-blocks
 printf '\n[%u] Launching Mixtool... %s\n' $$ "$(date '+%F %T %Z')" >>"$appdir/output.log"
 python3 -BEsu "$appdir/src/__main__.py" "$@" >>"$appdir/output.log" 2>&1
 printf '[%u] Mixtool has quit with EXIT CODE %u.\n\n' $$ $? >>"$appdir/output.log"
-unset LC_ALL G_SLICE
-if [ -n "$(command -v xdg-open)" ]; then
-	exec xdg-open "$appdir/output.log"
-elif [ -n "$(command -v open)" ]; then
-	exec open "$appdir/output.log"
-fi
