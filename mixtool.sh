972740ff7a46eafbeff86e71b67924aee6c9081b
Bachsau <dev@bachsau.com>
2018-04-25 22:51:05 +0200

    Reintroduce opening "output.log" only on errors

--- mixtool.sh
+++ mixtool.sh
@@ -5,4 +5,13 @@ export LC_ALL=en_US.UTF-8
 export G_SLICE=debug-blocks
 printf '\n[%u] Launching Mixtool... %s\n' $$ "$(date '+%F %T %Z')" >>"$appdir/output.log"
 python3 -BEsu "$appdir/src/__main__.py" "$@" >>"$appdir/output.log" 2>&1
-printf '[%u] Mixtool has quit with EXIT CODE %u.\n\n' $$ $? >>"$appdir/output.log"
+status=$?
+printf '[%u] Mixtool has quit with EXIT CODE %u.\n\n' $$ $status >>"$appdir/output.log"
+unset LC_ALL G_SLICE
+if [ $status -ne 0 ]; then
+	if [ -n "$(command -v xdg-open)" ]; then
+		exec xdg-open "$appdir/output.log"
+	elif [ -n "$(command -v open)" ]; then
+		exec open "$appdir/output.log"
+	fi
+fi
